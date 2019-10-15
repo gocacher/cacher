a8daa6b245f5cdb44b31ca7d1b81f5762dee061e
godcong <jumbycc@163.com>
2019-10-15 14:18:57 +0800

    fix GetD was catch error

--- cacher.go
+++ cacher.go
@@ -9,15 +9,13 @@ type Cacher interface {
 	//get a value from cache with default
 	GetD(key string, v interface{}) interface{}
 	//set a value to cache
-	Set(key string, val interface{}) (Cacher, error)
-	//set a value with no error
-	//MustSet(key string, val interface{}) Cacher
+	Set(key string, val interface{}) error
 	//set a value with ttl
-	SetWithTTL(key string, val interface{}, ttl int64) (Cacher, error)
+	SetWithTTL(key string, val interface{}, ttl int64) error
 	//check the value is exist
 	Has(key string) (bool, error)
 	//delete a value
-	Delete(key string) (Cacher, error)
+	Delete(key string) error
 	//delete a value with no error
 	//MustDelete(key string) Cacher
 	//clear all the values
@@ -25,9 +23,9 @@ type Cacher interface {
 	//get multi values
 	GetMultiple(keys ...string) (map[string]interface{}, error)
 	//set multi values with [key]value
-	SetMultiple(values map[string]interface{}) (Cacher, error)
+	SetMultiple(values map[string]interface{}) error
 	//delete multi values
-	DeleteMultiple(keys ...string) (Cacher, error)
+	DeleteMultiple(keys ...string) error
 	//return the last error
 	//Error() error
 }
