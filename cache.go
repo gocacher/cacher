1498da245e3d499b49064d5600e70c5ae47317d4
godcong <jumbycc@163.com>
2019-10-15 13:45:58 +0800

    add cacher

---
+++ cache.go
@@ -0,0 +1,59 @@
+package cacher
+
+//var cache sync.Map
+var cache Cacher
+
+/*RegisterCache register cache to map */
+func Register(c Cacher) {
+	cache = c
+}
+
+//Get get value
+func Get(key string) (interface{}, error) {
+	return cache.Get(key)
+}
+
+//GetD get value ,if not found return a default value
+func GetD(key string, v interface{}) (interface{}, error) {
+	return cache.GetD(key, v)
+}
+
+//Set set value
+func Set(key string, val interface{}) (Cacher, error) {
+	return cache.Set(key, val)
+}
+
+//SetWithTTL set value with time to life
+func SetWithTTL(key string, val interface{}, ttl int64) (Cacher, error) {
+	return cache.SetWithTTL(key, val, ttl)
+}
+
+//Has check value
+func Has(key string) (bool, error) {
+	return cache.Has(key)
+}
+
+//Delete delete value
+func Delete(key string) (Cacher, error) {
+	return cache.Delete(key)
+}
+
+//Clear clear all
+func Clear() error {
+	return cache.Clear()
+}
+
+//GetMultiple get multiple value
+func GetMultiple(keys ...string) (map[string]interface{}, error) {
+	return cache.GetMultiple(keys...)
+}
+
+//SetMultiple set multiple value
+func SetMultiple(values map[string]interface{}) (Cacher, error) {
+	return cache.SetMultiple(values)
+}
+
+//DeleteMultiple delete multiple value
+func DeleteMultiple(keys ...string) (Cacher, error) {
+	return cache.DeleteMultiple(keys...)
+}
--- cacher.go
+++ cacher.go
@@ -5,13 +5,13 @@ type Cacher interface {
 	//get a value
 	Get(key string) (interface{}, error)
 	// get a value from cache with no error
-	MustGet(key string) interface{}
+	//MustGet(key string) interface{}
 	//get a value from cache with default
 	GetD(key string, v interface{}) (interface{}, error)
 	//set a value to cache
 	Set(key string, val interface{}) (Cacher, error)
 	//set a value with no error
-	MustSet(key string, val interface{}) Cacher
+	//MustSet(key string, val interface{}) Cacher
 	//set a value with ttl
 	SetWithTTL(key string, val interface{}, ttl int64) (Cacher, error)
 	//check the value is exist
@@ -19,13 +19,15 @@ type Cacher interface {
 	//delete a value
 	Delete(key string) (Cacher, error)
 	//delete a value with no error
-	MustDelete(key string) Cacher
+	//MustDelete(key string) Cacher
 	//clear all the values
-	Clear()
+	Clear() error
 	//get multi values
 	GetMultiple(keys ...string) (map[string]interface{}, error)
 	//set multi values with [key]value
 	SetMultiple(values map[string]interface{}) (Cacher, error)
 	//delete multi values
 	DeleteMultiple(keys ...string) (Cacher, error)
+	//return the last error
+	//Error() error
 }
